713b8cebda8b97ddf844b94625db2394dcf77257
Mike Tung <miketung2013@gmail.com>
2017-11-18 16:10:13 -0500

    #1 added initial scaffolding

--- coder_directory_api/api.py
+++ coder_directory_api/api.py
@@ -8,16 +8,16 @@ MIT License, see LICENSE for details.
 import flask
 import settings
 
-app = flask.Flask('__name__')
+api = flask.Flask('__name__')
 
 
-@app.route('/')
+@api.route('/')
 def home():
     return 'success!'
 
 
 if __name__ == '__main__':
-    app.run(host=settings.HOST,
+    api.run(host=settings.HOST,
             port=settings.PORT,
             debug=settings.DEBUG,
             threaded=settings.MULTITHREADING)
---
+++ coder_directory_api/tests/api/test_api.py
@@ -0,0 +1,26 @@
+"""
+Tests for Coder Directory api
+
+Copyright (c) 2017 by Mike Tung.
+MIT License, see LICENSE for details.
+"""
+import unittest
+from api import api
+
+
+class ApiTest(unittest.TestCase):
+    def setUp(self):
+        """Sets up api prior to performing each test"""
+        api.config['TESTING'] = True
+        api.config['WTF_CRSF_ENABLED'] = False
+        self.api = api.test_client()
+
+    def tearDown(self):
+        """Clean up protocol for api after each test"""
+        self.app = None
+
+    def test_home(self):
+        result = self.api.get('/')
+        self.assertEquals(result.status_code,
+                          200,
+                          msg='Expected 200 status code')
