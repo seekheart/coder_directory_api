28547063e32327a647f9a96cfaa8686c0168d7ab
Mike Tung <miketung2013@gmail.com>
2017-11-24 16:05:55 -0500

    #9 made changes to BASE URL so that the default base url won't crash the tests

--- coder_directory_api/settings.py
+++ coder_directory_api/settings.py
@@ -13,7 +13,7 @@ HOST = os.environ.get('API_URI', '0.0.0.0')
 PORT = os.environ.get('API_PORT', 3000)
 SECRET = os.environ.get('API_SECRET', 'coder_directory_api/dev_settings.json')
 ENV = os.environ.get('API_ENV', 'DEV')
-BASE_URL = os.environ.get('API_BASE_URL', None)
+BASE_URL = os.environ.get('API_BASE_URL', '/dev')
 
 with open(SECRET, 'r') as s:
     creds = json.load(s)
